c4100536804d4dc1e8b31eee61801adf318bda85
LoïcDev <150665673+takamdev@users.noreply.github.com>
2024-10-09 21:33:54 +0100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -220,7 +220,7 @@ const logOut = ()=>{
       <nav className="navbar navbar-expand-lg bg-body-tertiary fixed-top">
           <div className="container-fluid">
             <Link className="navbar-brand fw-bold" href="/">
-              <img className="w-50" src="/public/assets/logo/1.png" alt="image" />
+              <img className="w-50" src="/assets/logo/1.png" alt="image" />
             </Link>
             <button className={`navbar-toggler`} type="button" id="navbarToggle" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
